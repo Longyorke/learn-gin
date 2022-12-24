0847be015e06178d815063aa047c57acd648995b
LongYorke <1269217124@qq.com>
2022-12-24 18:19:00 +0800

    feat: GET方法返回helloworld

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
-	fmt.Println("hello")
+	engine := gin.Default()
+	engine.GET("/hello", func(ctx *gin.Context) {
+		//返回数据
+		//type any = interface{}
+		//type H map[string]any
+		ctx.JSON(200, gin.H{
+			"name": "hello world",
+		})
+	})
+	err := engine.Run(":8082")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
