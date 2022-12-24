9bee116bf7742fc79c075e0dc982a95f611f21ff
LongYorke <1269217124@qq.com>
2022-12-24 22:09:57 +0800

    feat: 基础请求方法

--- helloworld/main.go
+++ helloworld/main.go
@@ -6,16 +6,35 @@ import (
 )
 
 func main() {
-	engine := gin.Default()
-	engine.GET("/hello", func(ctx *gin.Context) {
+	Router := gin.Default()
+	Router.GET("/hello", func(ctx *gin.Context) {
 		//返回数据
 		//type any = interface{}
 		//type H map[string]any
 		ctx.JSON(200, gin.H{
-			"name": "hello world",
+			"name": "LongYorke",
 		})
 	})
-	err := engine.Run(":8082")
+	Router.POST("/user", func(ctx *gin.Context) {
+		ctx.JSON(200, "POST")
+	})
+	Router.DELETE("/user", func(ctx *gin.Context) {
+		ctx.JSON(200, "DELETE")
+	})
+	Router.PUT("/user", func(ctx *gin.Context) {
+		ctx.JSON(200, "PUT")
+	})
+	Router.PATCH("/user", func(ctx *gin.Context) {
+		ctx.JSON(200, "PATCH")
+	})
+	Router.GET("/user", func(ctx *gin.Context) {
+		ctx.JSON(200, "GET")
+	})
+	Router.Any("/any", func(ctx *gin.Context) {
+		ctx.JSON(200, map[string]any{"method": "Any"})
+	})
+
+	err := Router.Run(":8082")
 	if err != nil {
 		log.Fatalln(err)
 	}
