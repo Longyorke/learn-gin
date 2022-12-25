e7ddd57870576c9b1b3c75c978e216699108a2ff
LongYorke <1269217124@qq.com>
2022-12-25 21:48:09 +0800

    feat: 请求数组参数

--- query/main.go
+++ query/main.go
@@ -7,20 +7,20 @@ import (
 )
 
 type User struct {
-	Id      int64  `form:"id"` // 注意是反引号,内部值为字符串
-	Name    string `form:"name"`
-	Address string `from:"address" binding:"required"`
-	//tag加上binding:"required"后.BindQuery()会返回400
+	Id      int64    `form:"id"` // 注意是反引号,内部值为字符串
+	Name    string   `form:"name"`
+	Address []string `form:"address" binding:"required"`
+	//tag加上binding:"required"后.BindQuery()返回400，ShouldBindQuery()返回200
 }
 
 func main() {
 	Router := gin.Default()
-	// http://localhost:8082/user/save?id=123456&name=LongYorke
+	// http://localhost:8082/user/save?address=Beijing&address=Shanghai
 	Router.GET("/user/save", func(ctx *gin.Context) {
 		var user User
-		err := ctx.ShouldBindQuery(&user)
+		err := ctx.ShouldBindQuery(&user) // 绑定类型方式
 		if err != nil {
-			log.Print(err)
+			log.Println(err)
 		}
 		ctx.JSON(http.StatusOK, user)
 	})
