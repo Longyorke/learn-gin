46fecbfca11eeff025fccd0185e620dce11ff711
LongYorke <1269217124@qq.com>
2022-12-26 10:42:43 +0800

    feat: Post JSON参数

--- post_query/main.go
+++ post_query/main.go
@@ -6,24 +6,43 @@ import (
 	"net/http"
 )
 
+//	type User struct {
+//		Id         int64             `form:"id"`
+//		Name       string            `form:"name"`
+//		Address    string            `form:"address"`
+//		AddressMap map[string]string `form:"addressMap"`
+//	}
 type User struct {
-	Id         int64             `form:"id"`
-	Name       string            `form:"name"`
-	Address    string            `form:"address" bind:"required"`
-	AddressMap map[string]string `form:"addressMap"`
+	Address    []string          `json:"address"`
+	AddressMap map[string]string `json:"addressMap"`
+	ID         int64             `json:"id"`
+	Name       string            `json:"name"`
 }
+
+//type AddressMap struct {
+//	Company string `json:"company"`
+//}
 
 func main() {
 	Router := gin.Default()
 	// http://localhost:8082/user/save
+	//Router.POST("/user/save", func(ctx *gin.Context) {
+	//	var user User
+	//	err := ctx.ShouldBind(&user) // 绑定表单
+	//	if err != nil {
+	//		log.Println(err)
+	//	}
+	//	addressMap := ctx.PostFormMap("addressMap") // 单独获取map
+	//	user.AddressMap = addressMap
+	//
+	//	ctx.JSON(http.StatusOK, user)
+	//})
 	Router.POST("/user/save", func(ctx *gin.Context) {
 		var user User
-		err := ctx.ShouldBind(&user) // 绑定表单
+		err := ctx.ShouldBindJSON(&user) // 绑定表单
 		if err != nil {
 			log.Println(err)
 		}
-		addressMap := ctx.PostFormMap("addressMap")
-		user.AddressMap = addressMap
 
 		ctx.JSON(http.StatusOK, user)
 	})
